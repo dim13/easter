33f875fc4cfdacc7be9d36186002544a84bd5c25
Dimitri Sokolyuk <demon@dim13.org>
2019-03-28 14:58:49 +0100

    add test for date

---
+++ date_test.go
@@ -0,0 +1,13 @@
+package calendar
+
+import (
+	"testing"
+	"time"
+)
+
+func TestDate(t *testing.T) {
+	d := date(2009, 314)
+	if d.Day() != 10 || d.Month() != time.November || d.Year() != 2009 {
+		t.Errorf("got %v, want 2009-11-10", d)
+	}
+}
