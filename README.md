b1bb0fd6bc9fa271f82006ddde09571993008d37
Dimitri Sokolyuk <demon@dim13.org>
2019-03-29 10:48:19 +0100

    update readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# Calendar computations
+
+[![GoDoc](https://godoc.org/github.com/dim13/calendar?status.svg)](https://godoc.org/github.com/dim13/calendar)
+[![Build Status](https://travis-ci.org/dim13/calendar.svg?branch=master)](https://travis-ci.org/dim13/calendar)
--- day.go
+++ day.go
@@ -19,3 +19,18 @@ import "time"
 func Parse(s string) time.Time {
 	return time.Time{}
 }
+
+var (
+	// 1-based month, 0-based days, cumulative
+	daytab = [][]int{
+		{0, -1, 30, 58, 89, 119, 150, 180, 211, 242, 272, 303, 333, 364},
+		{0, -1, 30, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365},
+	}
+	days = []string{
+		"sun", "mon", "tue", "wed", "thu", "fri", "sat",
+	}
+	months = []string{
+		"jan", "feb", "mar", "apr", "may", "jun",
+		"jul", "aug", "sep", "oct", "nov", "dec",
+	}
+)
