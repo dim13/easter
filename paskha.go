3ccc137bd306d045458e61c93ff84e48640b7d52
Dimitri Sokolyuk <demon@dim13.org>
2019-03-29 10:02:45 +0100

    minor cleanup

--- paskha.go
+++ paskha.go
@@ -8,19 +8,12 @@ func Paskha(year int) time.Time {
 }
 
 func paskha(year int) int {
-	var a, b, c, d, e int
-	const (
-		x = 15
-		y = 6
-	)
-	var cumdays int
-
-	a = year % 19
-	b = year % 4
-	c = year % 7
-	d = (19*a + x) % 30
-	e = (2*b + 4*c + 6*d + y) % 7
-	cumdays = 31 + 28
+	a := year % 19
+	b := year % 4
+	c := year % 7
+	d := (19*a + 15) % 30
+	e := (2*b + 4*c + 6*d + 6) % 7
+	cumdays := 31 + 28
 	if isleap(year) {
 		cumdays++
 	}
--- pesach.go
+++ pesach.go
@@ -8,21 +8,15 @@ func Pesach(year int) time.Time {
 }
 
 func pesach(year int) int {
-	var a, b, y, cumdays int
-	var d float64
+	const t = 33.0 + 14.0/24.0
+	const l = (1.0 + 485.0/1080.0) / 24.0 / 19.0
+	const k = (29.0 + (12.0+793.0/1080.0)/24.0) / 19.0
 
-	const (
-		t = 33.0 + 14.0/24.0
-		l = (1.0 + 485.0/1080.0) / 24.0 / 19.0
-		k = (29.0 + (12.0+793.0/1080.0)/24.0) / 19.0
-	)
-
-	y = year + 3760
-
-	a = (12*y + 17) % 19
-	b = y % 4
-	d = (t - 10.0*k + l + 14.0) + k*float64(a) + float64(b)/4.0 - l*float64(y)
-	cumdays = int(d)
+	y := year + 3760
+	a := (12*y + 17) % 19
+	b := y % 4
+	d := (t - 10.0*k + l + 14.0) + k*float64(a) + float64(b)/4.0 - l*float64(y)
+	cumdays := int(d)
 
 	/* the postponement */
 	switch (cumdays + 3*y + 5*b + 5) % 7 {
